129a5f58b67d87ca748733094c57d516fc9276c9
hainv <hainv@flomail.net>
2024-04-24 17:33:54 +0700

    suport otpauth

---
+++ README.md
@@ -0,0 +1,14 @@
+# NodeJS Two-Factor Authentication (2FA)
+
+## Requirements
+
+* **Nodejs > v12.0.x**
+
+Clone project and run test.
+
+```
+$ git clone https://git_url_clone <project_dir>
+$ cd <project_dir>
+$ npm install
+$ node src/server.js
+```
---
+++ src/helpers/2fa-otpauth.js
@@ -0,0 +1,61 @@
+import qrcode from 'qrcode'
+import * as OTPAuth from "otpauth";
+
+const generateUniqueSecret = () => {
+  const secret = new OTPAuth.Secret({
+    size: 32
+  })
+  
+  return secret.base32 // non, base32, buffer is ok
+}
+
+const generateOTPToken = (username, serviceName, secret) => {
+  console.log('username', username);
+  console.log('serviceName', serviceName);
+  console.log('secret', secret);
+
+  const totp = new OTPAuth.TOTP({
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: secret,
+    issuer: serviceName,
+    label: username,
+  })
+
+  const uri = totp.toString()
+  console.log('uri', uri);
+
+  return uri
+}
+
+const verifyOTPToken = (token, secret) => {
+  let totp = new OTPAuth.TOTP({
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 30,
+    secret: secret
+  });
+  
+  let delta = totp.validate({ token, window: 1 });
+  console.log('delta', delta);
+
+  return delta !== null;
+}
+
+const generateQRCode = async (otpAuth) => {
+  try {
+    const QRCodeImageUrl = await qrcode.toDataURL(otpAuth)
+    return `<img src='${QRCodeImageUrl}' alt='qr-code-img-hainv' />`
+  } catch (error) {
+    console.log('Could not generate QR code', error)
+    return
+  }
+}
+
+export {
+  generateUniqueSecret,
+  verifyOTPToken,
+  generateOTPToken,
+  generateQRCode,
+}
